e307d2842f6b6d6a81940ab1c9ab8fb85c79fdc3
Marla Brizel <marlabrizel@gmail.com>
2016-03-31 21:05:36 -0600

    Finish initial functionality

--- spec/genius/blocker_spec.rb
+++ spec/genius/blocker_spec.rb
@@ -8,28 +8,21 @@ describe Genius::Blocker do
   let(:app) do
     Rack::Builder.new do
       use Genius::Blocker
-      run  lambda { |env| [301, {"Location" => desired_url}, ["Moved Permanently"]] } 
+      run  lambda { |env| [301, {}, [<<-EOT]] }
+        <html>
+          <head>
+          </head>
+        </html>
+      EOT
     end
   end
 
-  let(:desired_url) { "www.google.com" }
+  let(:url) { "www.google.com" }
   let(:request) { Rack::MockRequest.new(app) }
   let(:response) { request.get(url) }
 
-  context 'when the uri is prepended with genius.it' do
-    let(:url) { "genius.it/www.google.com" }
-
-    it 'redirects when the uri is prepended with genius.it' do
-      expect(response.status).to eq(301)
-      expect(response.headers["Location"]).to eq(desired_url)
-    end
-  end
-
-  context 'when the uri is not prepended with genius.it' do
-    let(:url) { "www.google.com" }
-    it 'passes through without a genius uri' do
-      expect(response.status).to eq(200)
-      expect(response.headers["Location"]).to be_nil
-    end
+  it 'adds a script tag to redirect' do
+    document = Oga.parse_html(response.body)
+    expect(document.css('head > script').size).to eq 1
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'genius/blocker'
 require 'rack/mock'
 require 'pry'
+require 'oga'

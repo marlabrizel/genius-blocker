1d3a8075f037c6bf8e064c73f71b604ce184765d
Marla Brizel <marlabrizel@gmail.com>
2016-03-31 21:58:00 -0600

    Short circuit if content is not html
    Add check to short-circuit redirect logic if content type is something
    other than html

--- lib/genius/blocker.rb
+++ lib/genius/blocker.rb
@@ -18,11 +18,20 @@ module Genius
 
     def call(env)
       status, headers, body = @app.call(env)
+      return [status, headers, body] unless html?(headers)
+
       response = Rack::Response.new([], status, headers)
       body.each do |fragment|
         response.write fragment.gsub(%r{</head>}, "#{REDIRECT_SCRIPT}</head>")
       end
+
       response.finish
+    end
+
+    private
+
+    def html?(headers)
+      headers['Content-Type'] =~ /html/
     end
   end
 end
